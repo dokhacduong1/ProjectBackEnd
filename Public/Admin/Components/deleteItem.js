24a97a09c0790b531a2da827d2078234395e0118
dokhacduong3 <dokhacduong3@gmail.com>
2023-09-21 22:07:44 +0700

    ok8

--- Public/Admin/Components/deleteItem.js
+++ Public/Admin/Components/deleteItem.js
@@ -1,10 +1,12 @@
 export function DeleteItem() {
+   
     const buttonDelete = document.querySelectorAll(`[button-delete]`);
     if(buttonDelete.length>0){
         const formDelete = document.querySelector("#form-delete-status");
         const path = formDelete.getAttribute("data-path");
         buttonDelete.forEach(button=>{
             button.addEventListener("click",()=>{
+                console.log("ok")
                 const isComfirm = confirm("Bạn Có Chắc Muốn Xóa Sản Phẩm Này?")
                 if(isComfirm){
                     const id = button.getAttribute("data-id");
